354dabe62d6b84bf6b5d2dd6c28d4622429c36b2
ManuelAmoresGonzalez <amoresgonzalezmanuel@gmail.com>
2022-04-07 23:38:52 -0600

    Get and Post functions with firebase

--- src/components/Administration.jsx
+++ src/components/Administration.jsx
@@ -1,27 +1,51 @@
 import React from 'react'
 import { useNavigate} from "react-router-dom";
+import axios from 'axios';
+import { getDatabase, ref, set } from "firebase/database";
 
 const Administration = () => {
   const navigate = useNavigate();
+  // fetch('https://datosplantilla-46cf4-default-rtdb.firebaseio.com/.json')
+  // .then(response => response.json())
+  // .then(data => console.log(data));
+  const peticion = async () => {
+    const { data } = await axios.get(
+      "https://datosplantilla-46cf4-default-rtdb.firebaseio.com/.json"
+    );
+  
+    return data;
+  };  
+  const data = peticion().then(console.log);
+
+  
+
+  function writeUserData() {
+    const db = getDatabase();
+    set(ref(db, 'carros/persona'), {
+      'ssff': "ds"
+    });
+  }
+  
+
+
+
   return (
-    <div style={{
-        backgroundColor: 'blue',
-        width: '100%',
-        height: '100px',
-        textAlign: 'center'}}>
+    <div className='bg-secondary row' >      
 
-      
-
-      <button class="btn waves-effect waves-light" type="submit" name="action" onClick={() => navigate('/productsview')}> Productos
-        <i class="material-icons right">send</i>
+      <button className="btn waves-effect waves-light col s12 m4 l2" type="submit" name="action" onClick={() => writeUserData()}> Crear Producto
+        <i class="material-icons right">assessment</i>
       </button>
 
-      <button class="btn waves-effect waves-light" type="submit" name="action" onClick={() => navigate('/createcarrousel')}> Productos
-        <i class="material-icons right">send</i>
+      <button class="btn waves-effect waves-light col s12 m4 l2" type="submit" name="action" onClick={() => navigate('/createcarrousel')}> Crear Carrousel
+        <i class="material-icons right">burst_mode</i>
       </button>
 
-      <button class="btn waves-effect waves-light" type="submit" name="action" onClick={() => navigate('/createcategory')}> Productos
-        <i class="material-icons right">send</i>
+      <button class="btn waves-effect waves-light col s12 m4 l2" type="submit" name="action" onClick={() => navigate('/createcategory')}> Crear Logo
+        <i class="material-icons right">folder_shared</i>
+      </button>
+
+      <button class="btn waves-effect waves-light col s12 m4 l2" type="submit" name="action" onClick={() => navigate('/createcategory')}> Crear Categoría
+        <i class="material-icons right">library_add</i>
       </button>
 
     </div>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -68,23 +68,3 @@ const Register = () => {
 }
 
 export default Register
-
-
-function Comment (props) {
-  return (
-    <div className="Comment">
-      <div className="UserInfo">
-        <Avatar user={props.author} />
-        <div className="UserInfo-name">
-          {props.author.name}
-        </div>
-      </div>
-      <div className="Comment-text">
-        {props.text}
-      </div>
-      <div className="Comment-date">
-        {formatDate(props.date)}
-      </div>
-    </div>
-  );
-}
